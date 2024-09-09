fb5c2b5425ca8415c82a41356d699c658d27c31c
Fabian Druschke <fdruschke@outlook.com>
2024-09-09 21:06:31 +0200

    Changed banner version handling to include commit hash in banner

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,15 +4,29 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.37
-NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
+# Verwende das Submodul unter external/nwipe auf der aes-ctr Branch
+NWIPE_VERSION = aes-ctr
+NWIPE_SITE = NO_SITE
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
+# Der Pfad, in dem das Submodul initialisiert wurde
+NWIPE_SUBMODULE_PATH = $(TOPDIR)/external/nwipe
+
 define NWIPE_INITSH
-	(cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
+	(cd $(NWIPE_SUBMODULE_PATH) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(NWIPE_SUBMODULE_PATH) && ./002-nwipe-banner-patch.sh && PATH="../../host/bin:${PATH}" ./autogen.sh);
 endef
 
 NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
 
+# Deaktiviere den Download-Schritt
+define DOWNLOAD_NWIPE
+	true
+endef
+
+# Kopiere den Quellcode aus dem Submodul an den Build-Pfad
+define NWIPE_EXTRACT_CMDS
+	rsync -a $(NWIPE_SUBMODULE_PATH)/ $(@D)/
+endef
 
 $(eval $(autotools-package))
+
