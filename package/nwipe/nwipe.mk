98e70c906d2247999e1665ffa2975df174432595
Fabian Druschke <fdruschke@outlook.com>
2025-11-23 21:13:23 +0100

    package/nwipe: add support for selecting Git reference (branch/tag/commit)
    This adds support for building nwipe from an arbitrary Git ref instead of a
    hard-coded commit. A new Kconfig choice allows selecting between:
    
      - stable commit (default)
      - master branch
      - custom Git reference
    
    If the custom option is selected, a user-provided string is used as the
    Git ref (branch, tag or commit hash). The nwipe.mk now consumes the
    resulting BR2_PACKAGE_NWIPE_GIT_REF value via NWIPE_VERSION.
    
    This makes it easier to test development branches, experimental patches
    or upstream changes without modifying the package infrastructure.

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,28 +4,20 @@
 #
 ################################################################################
 
-# Instead of v0.38, we now use the desired commit (e.g., 051e1aa)
-NWIPE_VERSION = 051e1aa
+NWIPE_VERSION = $(call qstrip,$(BR2_PACKAGE_NWIPE_GIT_REF))
 
-# Specify the Git source:
-#  - NWIPE_SITE points to the repository
-#  - NWIPE_SITE_METHOD = git ensures that Buildroot performs a Git checkout
-NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe.git
+NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe
 NWIPE_SITE_METHOD = git
 
-# Dependencies remain the same (adjust them if you need additional ones)
-NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
+NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils libconfig
 
-# This hook copies your patch script (banner patch) into the source directory
-# and runs autogen.sh before the actual configure/make starts.
 define NWIPE_INITSH
-        (cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) \
-                && ./002-nwipe-banner-patch.sh \
-                && PATH="../../host/bin:${PATH}" ./autogen.sh);
+	(cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) \
+		&& ./002-nwipe-banner-patch.sh \
+		&& PATH="../../host/bin:${PATH}" ./autogen.sh);
 endef
 
 NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
 
-# Include the Buildroot autotools package framework, 
-# which takes care of configure/make/make install.
 $(eval $(autotools-package))
+
