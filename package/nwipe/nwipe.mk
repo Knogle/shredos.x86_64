86e075aae4769483aff101541d1bdfa1b5425c31
Fabian Druschke <fdruschke@outlook.com>
2025-11-25 12:14:38 +0100

    package/nwipe: drop explicit master option and clarify Git revision semantics
    Buildroot's git download backend does not automatically track moving
    branches once the initial fetch has completed. Keeping a "master branch"
    menu entry therefore suggests semantics (always up-to-date master) that
    the build system does not actually guarantee.
    
    This change removes the explicit "Upstream master branch" option and
    rewords the Git revision help text to only mention commit SHA-1 values
    and tags. Users can still enter a branch name as a Git revision if they
    know what they are doing, but the UI no longer advertises branch-based
    tracking as a supported or reliable mode.

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -5,12 +5,8 @@
 ################################################################################
 
 # Select the Git reference based on the Kconfig choice.
-# We only use the custom string when the corresponding bool is enabled,
-# similar to how coreboot handles the SeaBIOS git revision option.
 ifeq ($(BR2_PACKAGE_NWIPE_VERSION_STABLE),y)
 NWIPE_VERSION = 051e1aa
-else ifeq ($(BR2_PACKAGE_NWIPE_VERSION_MASTER),y)
-NWIPE_VERSION = master
 else ifeq ($(BR2_PACKAGE_NWIPE_VERSION_GIT_REVISION),y)
 NWIPE_VERSION = $(call qstrip,$(BR2_PACKAGE_NWIPE_GIT_REVISION))
 else
@@ -18,24 +14,16 @@ else
 NWIPE_VERSION = 051e1aa
 endif
 
-# Default Git repository URL. This ensures NWIPE_SITE is never empty,
-# even if BR2_PACKAGE_NWIPE_SITE is not set in the configuration.
+# Default Git repository URL (never empty).
 NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe.git
-
-# If the Kconfig variable is non-empty, override the default.
 ifneq ($(call qstrip,$(BR2_PACKAGE_NWIPE_SITE)),)
 NWIPE_SITE = $(call qstrip,$(BR2_PACKAGE_NWIPE_SITE))
 endif
 
 NWIPE_SITE_METHOD = git
 
-# Runtime and build-time dependencies for nwipe.
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils libconfig
 
-# Post-patch hook:
-#  - copy the banner patch script into the source tree
-#  - run the script
-#  - run autogen.sh with the proper host PATH before configure/make.
 define NWIPE_INITSH
 	(cd $(@D) && \
 		cp ../../../package/nwipe/002-nwipe-banner-patch.sh . && \
@@ -45,6 +33,5 @@ endef
 
 NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
 
-# Use the generic autotools infrastructure provided by Buildroot.
 $(eval $(autotools-package))
 
